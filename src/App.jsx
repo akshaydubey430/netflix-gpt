9abd8578b80f56ea8eee5ad8bdce09d5c56cc978
akshaydubey430 <akshaykumardubey023@gmail.com>
2025-04-09 23:51:46 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="font-bold  m-4 p-4 text-amber-600">
+      All the best for your exams!!
+    </div>
+  );
+}
+
+export default App;
